fcd2955669e7f3a531d1b6a306b0d2525a6763a9
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2021-12-25 07:04:21 +0530

    idk

--- backEnd/index.js
+++ backEnd/index.js
@@ -24,85 +24,84 @@ mongoose
     console.log(err);
   });
 
-const getData = async function (id) {
-  // tableinfo = [ [citations_all , citations_since 2016] , [hindex_all, hindex_since2016 ] , [i10index_all , i10index_since2016] ]
-  let tableinfo = [
-    [0, 0],
-    [0, 0],
-    [0, 0],
-  ];
+  const getData = async function(id){
+    
+    // tableinfo = [ [citations_all , citations_since 2016] , [hindex_all, hindex_since2016 ] , [i10index_all , i10index_since2016] ]
+    let tableinfo = [ [0,0],[0,0],[0,0]];
 
-  // graphinfo = [ [ year , no_of_citations ] , ... ]
-  let graphinfo = [];
+    // graphinfo = [ [ year , no_of_citations ] , ... ]
+    let graphinfo = [];
 
-  const photoUrl =
-    "https://scholar.googleusercontent.com/citations?view_op=view_photo&user=" +
-    id;
-  const url = "https://scholar.google.com/citations?user=" + id + "&hl=en";
-  try {
-    // gets the html data from the url
-    const response = await axios.get(url);
+    const photoUrl = "https://scholar.googleusercontent.com/citations?view_op=view_photo&user=" + id;
+    const url = 'https://scholar.google.com/citations?user=' + id + '&hl=en';
+    try{
+        // gets the html data from the url
+        const response = await axios.get(url);
 
-    // loads the html data into a cheerio function
-    const $ = cheerio.load(response.data);
+        // loads the html data into a cheerio function
+        const $ = cheerio.load(response.data);
 
-    // uses cheerio function to select name from html
-    const name = $("#gsc_prf_in").text();
-
-    // Selects and Traverses the table data and saves data into an array 'table info'
-    $("#gsc_rsb_st tbody")
-      .children()
-      .each((index, elem) => {
-        $(elem)
-          .children(".gsc_rsb_std")
-          .each((j, elem2) => {
-            tableinfo[index][j] = parseInt($(elem2).text());
-          });
-      });
-
-    // Traverses graph data 'year' and add it to the array 'graphinfo
-    $(".gsc_md_hist_b .gsc_g_t").each((index, elem) => {
-      graphinfo.push([parseInt($(elem).text()), 0]);
-    });
-
-    const total = graphinfo.length;
-    const Rlength = $(".gsc_md_hist_b").children(" a ").length;
-    // Traverses no. of citations in that particular year and saves it to array
-    $(".gsc_g_a").each((index, elem) => {
-      //Exceptional case : When no. of citation in any year is zero, then its html element is not present.
-      // In this case, we match the correct values by using z-index of element
-      if (Rlength !== total) {
-        //gets the string of style attribute
-        var str = $(elem).attr("style");
-        // slices the last two characters of the string, example: '10' or ':8'
-        var x = str.slice(-2);
-        //if x is ':8', then it becomes integer 8
-        if (isNaN(x)) {
-          x = parseInt(x[1]);
-        } else {
-          x = parseInt(x);
+        // uses cheerio function to select name from html
+        const name = $("#gsc_prf_in").text();
+        if( name == ""){
+            throw "User not found";
         }
-        // adds the value to its correct index
-        graphinfo[total - x][1] = parseInt($(elem).text());
-      }
-      // normal case
-      else {
-        graphinfo[index][1] = parseInt($(elem).text());
-      }
-    });
+        console.log("name = " + name);
 
-    // returns data object
-    return {
-      name: name,
-      tableData: tableinfo,
-      graphData: graphinfo,
-      photoUrl: photoUrl,
-    };
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
+        // Selects and Traverses the table data and saves data into an array 'table info'
+        $("#gsc_rsb_st tbody").children().each((index, elem) => {
+            $(elem).children('.gsc_rsb_std').each((j, elem2)=> {
+                tableinfo[index][j] = parseInt($(elem2).text());
+                
+            })
+        });
+
+        // Traverses graph data 'year' and add it to the array 'graphinfo
+        $(".gsc_md_hist_b .gsc_g_t").each( (index, elem) => {
+            graphinfo.push([parseInt($(elem).text()),0])
+        });
+
+        console.log(tableinfo);
+        console.log(graphinfo);
+        
+
+        const countStr = $(".gsc_g_a").first().attr("style");
+        const count = parseInt(countStr.split("z-index:").pop());
+        
+        // Traverses no. of citations in that particular year and saves it to array
+        $(".gsc_g_a").each((index,elem) => {
+            //gets the string of style attribute
+            var str = $(elem).attr("style");
+            console.log(str);
+            var splittedArray = str.split("z-index:");
+            var x = parseInt(splittedArray.pop());
+            console.log(x);
+            // adds the value to its correct index
+            graphinfo[count - x][1] = parseInt($(elem).text());
+        })
+
+        
+        // returns data object
+        return ({
+            name : name,
+            tableData : tableinfo,
+            graphData : graphinfo,
+            photoUrl : photoUrl,
+            status_code : response.status,
+            status_text : response.statusText
+        })
+    }catch (error){
+        console.log("Error");
+        console.log(error);
+        
+        if(error.response){
+            return ({status_code : error.response.status, status_text : error.response.statusText})
+        }
+        return ({status_code : 500, status_text : error})
+    }
+        
+}
+
 
 // Route    - /user/:id
 // Des      - To get a user data
@@ -110,15 +109,15 @@ const getData = async function (id) {
 // Method   - GET
 // Params   - id
 // Body    - none
-OurApp.get("/user/:id", async (req, res) => {
-  try {
-    const data = await getData(req.params.id);
-    console.log("express code continues");
-    return res.json(data);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+OurApp.get('/user/:id', async(req,res) => {
+    try{
+        const data = await getData(req.params.id);
+        console.log('express code continues');
+        return res.status(data.status_code).json(data);
+    }catch(error){
+        console.log(error);
+    }
+
 });
 
 OurApp.get("/", (request, response) => {
